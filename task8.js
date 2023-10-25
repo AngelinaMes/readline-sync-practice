c29dc40ad1d75a4a6869e972c68020c3933496ae
Ангелина Месяцына <angelmesss13@gmail.com>
2023-10-26 00:04:39 +0300

    Задача №8: Калькулятор расходов и доходов

---
+++ task8.js
@@ -0,0 +1,6 @@
+function calculateBalance(incomes, expenses) {
+    const totalIncome = incomes.reduce((acc, income) => acc + income, 0);
+  const totalExpenses = expenses.reduce((acc, expense) => acc + expense, 0);
+  const balance = totalIncome - totalExpenses;
+  return balance;
+}
