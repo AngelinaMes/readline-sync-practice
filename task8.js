29ea8a4b9483677bd6aaa61a1efb7ef588e299c9
AngelinaMes <144902781+AngelinaMes@users.noreply.github.com>
2023-12-18 18:48:20 +0300

    Update task8.js

--- task8.js
+++ task8.js
@@ -1,5 +1,5 @@
 function calculateBalance(incomes, expenses) {
-    const totalIncome = incomes.reduce((acc, income) => acc + income, 0);
+  const totalIncome = incomes.reduce((acc, income) => acc + income, 0);
   const totalExpenses = expenses.reduce((acc, expense) => acc + expense, 0);
   const balance = totalIncome - totalExpenses;
   return balance;
